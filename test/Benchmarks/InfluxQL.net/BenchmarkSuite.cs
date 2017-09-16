94f4d7a8db3a6b759d6db03c5c067c0572436b5a
Graham Ambrose <graham.ambrose@gmail.com>
2017-09-16 18:29:48 +0000

    Spike on using json reader

--- test/Benchmarks/InfluxQL.net/BenchmarkSuite.cs
+++ test/Benchmarks/InfluxQL.net/BenchmarkSuite.cs
@@ -1,19 +1,22 @@
 ﻿using System;
+using System.IO;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Benchmarks;
 using InfluxDB.InfluxQL.Client;
 using InfluxDB.InfluxQL.Tests.DataExplorationExamples.NoaaSampleData;
+using Newtonsoft.Json;
 
 // ReSharper disable CheckNamespace
 namespace InfluxDB.InfluxQL
 {
-    public class BenchmarkSuite: IBenchmarkSuite
+    public class BenchmarkSuite : IBenchmarkSuite
     {
         public async Task<double> SumTheWaterLevel(string endpoint, string database)
         {
             var h2o_feet = new WaterDepth();
 
-            var query = InfluxQuery.From(h2o_feet).Select(x=> new { x.water_level });
+            var query = InfluxQuery.From(h2o_feet).Select(x => new { x.water_level });
 
             var client = new InfluxQLClient(new Uri(endpoint), database);
 
@@ -23,6 +26,130 @@ namespace InfluxDB.InfluxQL
             foreach (var (time, values) in results)
             {
                 total += values.water_level;
+            }
+
+            return total;
+        }
+
+        public async Task<double> SumTheWaterLevelAlt(string endpoint, string database)
+        {
+            var h2o_feet = new WaterDepth();
+
+            var query = InfluxQuery.From(h2o_feet).Select(x => new { x.water_level });
+
+            var httpClient = new HttpClient { BaseAddress = new Uri(endpoint) };
+
+            var response = await httpClient.GetAsync($"query?db={Uri.EscapeDataString(database)}&q={Uri.EscapeDataString(query.Statement.Text)}");
+            response.EnsureSuccessStatusCode();
+
+            var resposeStream = await response.Content.ReadAsStreamAsync();
+            var reader = new StreamReader(resposeStream);
+            var jsonReader = new Newtonsoft.Json.JsonTextReader(reader);
+
+            double total = 0;
+
+            jsonReader.Read();
+
+            if (jsonReader.TokenType != JsonToken.StartObject)
+            {
+                throw new Exception();
+            }
+
+            jsonReader.Read();
+
+            if (jsonReader.TokenType != JsonToken.PropertyName && "results".Equals(jsonReader.Value))
+            {
+                throw new Exception();
+            }
+
+            jsonReader.Read();
+
+            if (jsonReader.TokenType != JsonToken.StartArray)
+            {
+                throw new Exception();
+            }
+
+            jsonReader.Read();
+
+            if (jsonReader.TokenType != JsonToken.StartObject)
+            {
+                throw new Exception();
+            }
+
+            jsonReader.Read();
+
+            if (jsonReader.TokenType != JsonToken.PropertyName && "statement_id".Equals(jsonReader.Value))
+            {
+                throw new Exception();
+            }
+
+            jsonReader.Skip();
+
+            jsonReader.Read();
+
+            if (jsonReader.TokenType != JsonToken.PropertyName && "statement_id".Equals(jsonReader.Value))
+            {
+                throw new Exception();
+            }
+
+            jsonReader.Read();
+
+            if (jsonReader.TokenType != JsonToken.StartArray)
+            {
+                throw new Exception();
+            }
+
+            jsonReader.Read();
+
+            if (jsonReader.TokenType != JsonToken.StartObject)
+            {
+                throw new Exception();
+            }
+
+            jsonReader.Read();
+
+            if (jsonReader.TokenType != JsonToken.PropertyName && "name".Equals(jsonReader.Value))
+            {
+                throw new Exception();
+            }
+
+            jsonReader.Skip();
+
+            jsonReader.Read();
+
+            if (jsonReader.TokenType != JsonToken.PropertyName && "columns".Equals(jsonReader.Value))
+            {
+                throw new Exception();
+            }
+
+            jsonReader.Skip();
+
+            jsonReader.Read();
+
+            if (jsonReader.TokenType != JsonToken.PropertyName && "values".Equals(jsonReader.Value))
+            {
+                throw new Exception();
+            }
+
+            jsonReader.Read();
+
+            if (jsonReader.TokenType != JsonToken.StartArray)
+            {
+                throw new Exception();
+            }
+
+            while (jsonReader.Read() && jsonReader.TokenType == JsonToken.StartArray)
+            {
+                jsonReader.ReadAsDateTime();
+
+                total += jsonReader.ReadAsDouble().Value;
+
+                jsonReader.Read();
+
+                if (jsonReader.TokenType != JsonToken.EndArray)
+                {
+                    throw new Exception();
+                }
             }
 
             return total;
--- test/Benchmarks/SumTheWaterLevel.cs
+++ test/Benchmarks/SumTheWaterLevel.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
 
 namespace Benchmarks
@@ -15,28 +14,34 @@ namespace Benchmarks
             return new InfluxDB.InfluxQL.BenchmarkSuite().SumTheWaterLevel(endpoint, database);
         }
 
-        [Benchmark(Description = "InfluxData.Net by pootzko")]
-        public Task<double> by_pootzko()
+        [Benchmark(Description = "Alt InfluxQL.net by gambrose")]
+        public Task<double> alt_by_gambrose()
         {
-            return new InfluxData.Net.InfluxDb.BenchmarkSuite().SumTheWaterLevel(endpoint, database);
+            return new InfluxDB.InfluxQL.BenchmarkSuite().SumTheWaterLevelAlt(endpoint, database);
         }
 
-        [Benchmark(Description = "InfluxDB Client for .NET by MikaelGRA")]
-        public Task<double> by_MikaelGRA()
-        {
-            return new Vibrant.InfluxDB.Client.BenchmarkSuite().SumTheWaterLevel(endpoint, database);
-        }
+        //[Benchmark(Description = "InfluxData.Net by pootzko")]
+        //public Task<double> by_pootzko()
+        //{
+        //    return new InfluxData.Net.InfluxDb.BenchmarkSuite().SumTheWaterLevel(endpoint, database);
+        //}
 
-        [Benchmark(Description = "InfluxDB.Client.Net by mvadu")]
-        public Task<double> by_mvadu()
-        {
-            return new AdysTech.InfluxDB.Client.Net.BenchmarkSuite().SumTheWaterLevel(endpoint, database);
-        }
+        //[Benchmark(Description = "InfluxDB Client for .NET by MikaelGRA")]
+        //public Task<double> by_MikaelGRA()
+        //{
+        //    return new Vibrant.InfluxDB.Client.BenchmarkSuite().SumTheWaterLevel(endpoint, database);
+        //}
 
-        [Benchmark(Description = "InfluxClient by danesparza")]
-        public Task<double> by_danesparza()
-        {
-            return new InfluxClient.BenchmarkSuite().SumTheWaterLevel(endpoint, database);
-        }
+        //[Benchmark(Description = "InfluxDB.Client.Net by mvadu")]
+        //public Task<double> by_mvadu()
+        //{
+        //    return new AdysTech.InfluxDB.Client.Net.BenchmarkSuite().SumTheWaterLevel(endpoint, database);
+        //}
+
+        //[Benchmark(Description = "InfluxClient by danesparza")]
+        //public Task<double> by_danesparza()
+        //{
+        //    return new InfluxClient.BenchmarkSuite().SumTheWaterLevel(endpoint, database);
+        //}
     }
 }
